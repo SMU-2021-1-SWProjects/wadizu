64100f95dcf325fbe28d069f884d38803eb45d10
c TM Jaesung Choi <skyler4784@gmail.com>
2021-05-10 01:23:50 +0900

    second commit add src folder and move python files into src add data folder and add gps_test.csv into data

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from stay_detection import stay_detection
 import pandas as pd
 import numpy as np
 
-data = pd.read_csv("C:/Users/skyle/Wadizu/gps.csv", sep= ',')
+data = pd.read_csv("C:/Users/skyle/Wadizu/data/gps_test.csv", sep= ',')
 gps_trajectory = np.array(data)
 
 clustered_gps, cluster_cnt = stay_detection(gps_trajectory)

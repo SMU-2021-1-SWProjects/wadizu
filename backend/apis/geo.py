f38397be340475af1a2dd0801e001af0f988ad85
Shane Oh <ohshane71@gmail.com>
2021-05-16 15:38:43 +0900

    added inline model

--- backend/apis/geo.py
+++ backend/apis/geo.py
@@ -1,3 +1,23 @@
+from typing_extensions import Required
 from flask_restx import Namespace, Resource, fields
 
-api = Namespace('geo', description='Geo related operations')
+api = Namespace('trajectory', description='Trajectory related operations')
+
+trajectory = api.model('Trajectory', {
+    'latitude': fields.String(Required=True),
+    'longtitude': fields.String(Required=True),
+    'zero': fields.String(Required=False),
+    'altitude': fields.String(Required=False),
+    'timestamp': fields.String(Required=True),
+    'date': fields.String(Required=False),
+    'time': fields.String(Required=False),
+})
+
+TRAJECTORY = []
+
+@api.route('/')
+class GeoList(Resource):
+    @api.doc('list trajectory')
+    @api.marshal_list_with(trajectory)
+    def get(self):
+        return TRAJECTORY

97da536310ecfffa3bb79839053d4c9eabf0628b
c TM Jaesung Choi <skyler4784@gmail.com>
2021-05-28 23:19:34 +0900

    seventh commit(back_end) complete uploading function

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,6 +1,4 @@
-import os
-from flask import Flask, redirect, url_for, request, render_template
-from werkzeug.utils import secure_filename
+from flask import Flask, request, render_template
 import numpy as np
 import pandas as pd
 from stay_detection import stay_detection
@@ -15,9 +13,9 @@ def render_main():
 
 @app.route('/clustering',methods=['POST'])
 def clustering(gps= None, clusters= None, clusters_cnt= None, clusters_time= None, gps_visualizing= None,cluster_visualizing= None):
-    render_template('app.html')
     if request.method == 'POST':
-        data = pd.read_csv("C:/Users/skyle/Wadizu/data/1day.csv", sep= ',', header= None)
+        
+        data = pd.read_csv('./data/file.filename', sep= ',', header= None)
         gps_trajectory = np.array(data)
 
         clustered_gps_trajectory, clustered_cnt_list = stay_detection(gps_trajectory)
@@ -39,16 +37,13 @@ def clustering(gps= None, clusters= None, clusters_cnt= None, clusters_time= Non
     
     return render_template('clustering.html', gps= merged_gps, clusters= merged_cluster_list, clusters_cnt= sorted_by_cluster_cnt, clusters_time= sorted_by_cluster_time, gps_visualizing= gps_lat_long, cluster_visualizing= clsuster_num_alat_along_distance_dict)
 
-# file이 submit되면 전달되는 페이지
-# upload.html에서 form이 제출되면 /file_uploaded로 옮겨지게 되어 있음.
-@app.route('/file_uploaded', methods = ['POST'])
+@app.route('/upload_done', methods= ['POST'])
 def upload_file():
     if request.method == 'POST': # POST 방식으로 전달된 경우
-        f = request.files['file1']
-        # 파일 객체 혹은 파일 스트림을 가져오고, html 파일에서 넘겨지는 값의 이름을 file1으로 했기 때문에 file1임. 
-        f.save(f'uploads/{secure_filename(f.filename)}') # 업로드된 파일을 특정 폴더에저장하고, 
-        data= pd.read_csv('uploads/{secure_filename(f.filename)}', header= None)
-        return data
+        file = request.files['data_file']
+        file.save('./data/file.filename') # 업로드된 파일을 특정 폴더에저장하고, 
+
+        return render_template('app.html')
     
 if __name__ == '__main__':
     app.run(port=8000, debug=True)

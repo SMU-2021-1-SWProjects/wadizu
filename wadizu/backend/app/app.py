91b1ed9fac1f8ce12e03d1c1cb834a521de18e53
Seonhyeok <reo6209@naver.com>
2021-06-02 13:06:59 +0900

    first commit add all files

---
+++ wadizu/backend/app/app.py
@@ -0,0 +1,50 @@
+from flask import Flask, request, render_template
+import numpy as np
+import pandas as pd
+from stay_detection import stay_detection
+from cluster_merging import cluster_merging
+from sort_descending_cluster_list import sort_descending_cluster_list
+import json
+
+app = Flask(__name__)
+ 
+@app.route('/')
+def render_main():
+    return render_template('app.html')
+
+@app.route('/clustering',methods=['POST'])
+def clustering(gps= None, clusters= None, clusters_cnt= None, clusters_time= None, gps_visualizing= None,cluster_visualizing= None):
+    if request.method == 'POST':
+        
+        data = pd.read_csv('./data/file.filename', sep= ',', header= None)
+        gps_trajectory = np.array(data)
+
+        clustered_gps_trajectory, clustered_cnt_list = stay_detection(gps_trajectory)
+        
+        merged_gps, merged_cluster_list = cluster_merging(clustered_gps_trajectory, clustered_cnt_list)
+        sorted_by_cluster_cnt = sort_descending_cluster_list(merged_cluster_list, 8)
+        sorted_by_cluster_time = sort_descending_cluster_list(merged_cluster_list, 3)
+        
+        gps_lat_long = merged_gps[:, 0:2]
+        
+        clster_num = merged_cluster_list[:, 0:1]
+        clster_alat_along = merged_cluster_list[:, 6:8]
+        clster_distance = merged_cluster_list[:, -2:]
+        clsuster_num_alat_along_distance = np.hstack((clster_num, clster_alat_along, clster_distance))
+        clsuster_num_alat_along_distance = np.delete(clsuster_num_alat_along_distance, -2, axis=1)
+        
+        clsuster_num_alat_along_distance_dict = pd.DataFrame(clsuster_num_alat_along_distance, columns=["cluster_num", "avg_latitude", "avg_longtitude", "farthest_distance"])
+
+        data_dict = clsuster_num_alat_along_distance_dict.to_dict()
+        return render_template('index.html', cluster_visualizing=json.dumps(data_dict))
+
+@app.route('/upload_done', methods= ['POST'])
+def upload_file():
+    if request.method == 'POST': # POST 방식으로 전달된 경우
+        file = request.files['data_file']
+        file.save('./data/file.filename') # 업로드된 파일을 특정 폴더에저장하고,
+
+        return render_template('app.html')
+    
+if __name__ == '__main__':
+    app.run(port=8000, debug=True)
---
+++ wadizu/backend/app/stay_detection.py
@@ -0,0 +1,57 @@
+import numpy as np
+import random as rd
+
+# gps_trajectory의 column은 (위도, 경도, 0, 고도, 타임스탬프, 날짜, 시간)
+def stay_detection(gps_trajectory):
+    clustered_gps_trajectory = np.copy(gps_trajectory)
+    cluster_list = np.empty(shape=(0, 10), dtype= np.float64)
+    eps = 0.000009 * 5
+    start_latitude = clustered_gps_trajectory[0][0]
+    start_longtitude = clustered_gps_trajectory[0][1]
+    sum_latitude = start_latitude
+    sum_longtitude = start_longtitude
+    start_timestamp = clustered_gps_trajectory[0][4]
+    end_timestamp = clustered_gps_trajectory[0][4]
+    interval_timestamp = 0
+    cluster_num = 1
+    cluster_cnt = 0
+    min_cluster_num = 2
+    
+    # cluster_num column 추가
+    clustered_gps_trajectory = np.insert(clustered_gps_trajectory, len(clustered_gps_trajectory[0]), 0, axis=1)
+    
+    for i in range(0, len(clustered_gps_trajectory), 1):
+
+        # 위도, 경도가 임계값 범위이면 같은 cluster
+        if abs(start_latitude - clustered_gps_trajectory[i][0]) <= eps and \
+            abs(start_longtitude - clustered_gps_trajectory[i][1]) <= eps:
+            clustered_gps_trajectory[i][-1] = cluster_num
+            cluster_cnt += 1
+            sum_latitude += clustered_gps_trajectory[i][0]
+            sum_longtitude += clustered_gps_trajectory[i][1]
+            
+            if clustered_gps_trajectory[i][4] > end_timestamp:
+                end_timestamp = clustered_gps_trajectory[i][4]
+        
+        # 다른 cluster로 지정
+        else:    
+            if cluster_cnt >= min_cluster_num:
+                interval_timestamp = abs(start_timestamp - end_timestamp)
+                cluster_list = np.insert(cluster_list, len(cluster_list), [cluster_num, start_timestamp, end_timestamp, interval_timestamp, start_latitude, start_longtitude, sum_latitude / cluster_cnt, sum_longtitude / cluster_cnt, cluster_cnt, rd.randint(1,20)], axis=0)
+
+            cluster_cnt = 1
+            cluster_num += 1
+            clustered_gps_trajectory[i][-1] = cluster_num
+            start_latitude = clustered_gps_trajectory[i][0]
+            start_longtitude = clustered_gps_trajectory[i][1]
+            sum_latitude = start_latitude
+            sum_longtitude = start_longtitude
+            start_timestamp = clustered_gps_trajectory[i][4]
+            end_timestamp = clustered_gps_trajectory[i][4]
+                
+        # 마지막 데이터 cluster_cnt_list에 삽입
+        if i == len(clustered_gps_trajectory) - 1:
+            interval_timestamp = abs(start_timestamp - end_timestamp)
+            cluster_list = np.insert(cluster_list, len(cluster_list), [cluster_num, start_timestamp, end_timestamp, interval_timestamp, start_latitude, start_longtitude, sum_latitude / cluster_cnt, sum_longtitude / cluster_cnt, cluster_cnt, rd.randint(1,20)], axis=0)
+    
+    return clustered_gps_trajectory, cluster_list
